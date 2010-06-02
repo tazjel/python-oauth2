44a94774976f5cff659c54b5c0854c0a85c7c91f
Zac Bowling <zac@zacbowling.com>
2010-06-02 15:24:59 -0700

    need to do a private release of my own to my customers and haven't had a pull in a while. i hope this doesn't break things later.

--- setup.py
+++ setup.py
@@ -3,14 +3,22 @@
 from setuptools import setup, find_packages
 
 setup(name="oauth2",
-      version="1.2.0",
+      version="1.2.0.1",
       description="Library for OAuth version 1.0a.",
       author="Joe Stump",
       author_email="joe@simplegeo.com",
+      maintainer="Zac Bowling",
+      maintainer_email="zac@zacbowling.com",
       url="http://github.com/simplegeo/python-oauth2",
       packages = find_packages(),
       install_requires = ['httplib2'],
       license = "MIT License",
       keywords="oauth",
       zip_safe = True,
-      tests_require=['nose', 'coverage', 'mox'])
+      tests_require=['nose', 'coverage', 'mox']
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Development Status :: 5 - Stable',
+          'Programming Language :: Python',
+      ]
+)
